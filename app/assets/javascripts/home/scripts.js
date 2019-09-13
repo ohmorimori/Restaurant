32665d88f7d437708b3143a9bc30a20fc9c848e2
大木杜生 <ohmori@ookimorionoMacBook.local>
2019-09-13 16:59:45 +0900

    implement ajax

---
+++ app/assets/javascripts/home/scripts.js
@@ -0,0 +1,28 @@
+
+var search_box = $('#search');
+
+search_box.click(function(){
+  var ajax = new XMLHttpRequest();
+  console.log("hi");
+  ajax.open("get", `https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=6463bc55622ce5ad6df3afe4bd4d9815&address=${search_box.text}`);
+  ajax.responseType = 'json';
+  ajax.send();
+  var html = "";
+
+  ajax.onload = function (e) {
+    console.log(e.target.response.rest);
+    for (var i = 0; i < e.target.response.rest.length; i++) {
+    	console.log(e.target.response.rest[i]);
+    	html +=  
+    	 '<div class="shops-index-item">'
+    	+	'<div class="shop-name">'
+    	+		'<%= link_to("店名{shop.shop_id}", "https://www.gnavi.co.jp/", target: "_blank") %>'
+    	+	'</div>'
+    	+'</div>'
+    }
+  }
+  
+  $('.container').append(html);
+
+});
+
