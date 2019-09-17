41d302668462bc0d2be08c9d283cb879a46a53a7
ohmorimori <a_pistol_costs_ten_cent@yahoo.co.jp>
2019-09-17 12:22:03 +0900

    get info from gnavi api in controller

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -3,6 +3,19 @@ require 'net/http'
 class ShopsController < ApplicationController
 
 	def index
+		url = "https://api.gnavi.co.jp/RestSearchAPI/v3/";
+		prms = URI.encode_www_form(
+			{
+			    keyid: "6463bc55622ce5ad6df3afe4bd4d9815",
+			   	address: params[:search]
+			}
+		)
 
+		parsed_url = URI.parse(url + "?" + prms)
+		@query = parsed_url.query
+		http = Net::HTTP.new(parsed_url.host, parsed_url.port)
+		http.use_ssl = (parsed_url.scheme == 'https')
+		response = http.get(parsed_url.request_uri)
+		@result = JSON.parse(response.body)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/search' => 'home#search'
   get 'shops/index' => 'shops#index'
   root 'home#top'
 
