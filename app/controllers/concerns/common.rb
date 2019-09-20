5c95fb866f4b1d2f43aafa7bdcabfda06293c441
ohmorimori <a_pistol_costs_ten_cent@yahoo.co.jp>
2019-09-20 17:40:16 +0900

    sort list by rating calculated with distance and average score of reviews

--- app/controllers/concerns/common.rb
+++ app/controllers/concerns/common.rb
@@ -27,7 +27,7 @@ module Common
 		http.use_ssl = (parsed_url.scheme == 'https')
 		response = http.get(parsed_url.request_uri)
 		result = JSON.parse(response.body)
-		return result
+
 	end
 	
 	def get_restaurants_info(search_word, latitude, longitude)
@@ -65,7 +65,7 @@ module Common
 				    latitude: latitude,
 				    longitude: longitude,
 				    range: 4,
-				    hit_per_page: 20
+				    hit_per_page: 10
 				}
 			)
 		elsif (params[:search].present?)
@@ -80,7 +80,71 @@ module Common
 
 		result = get_response_from_api(url, prms)
 		shops = result["rest"]
-		return shops
+		
+	end
+
+	def get_restaurants_review(shop_id)
+
+=begin
+		get restaurants from gnavi api 
+		Parameters
+		---------------
+		rest_id: string
+			string of id of restaurant
+			example
+			"gdmt407"
+		Returns
+		---------------
+		reviews: hash
+			reviews list of restaurants
+=end
+
+		key_id = "6463bc55622ce5ad6df3afe4bd4d9815"
+		url = "https://api.gnavi.co.jp/PhotoSearchAPI/v3/"
+		prms = ""
+		if (shop_id.present?)
+			prms = URI.encode_www_form(
+				{
+				#https://api.gnavi.co.jp/api/manual/restsearch/
+				    keyid: key_id,
+				    shop_id: shop_id,
+				}
+			)
+		end
+
+		result = get_response_from_api(url, prms)
+		result = result["response"]
+
+		reviews = []
+
+		if result.blank?
+			return reviews
+		end
+		#["@attributes", {"api_version"=>"v3"}] at index 0
+		#["total_hit_count", 3] at index 1
+		#["hit_per_page", 15] at index 2
+		#["0", {review}]  at index 3
+		#["1", {review}]  at index 4
+		#["2", {review}]  at index 5
+		record_starts_from = 3
+		result.each_with_index do |item, idx|
+			#record is from index 3
+			if idx >= record_starts_from
+			    reviews.push(item[1]["photo"])
+			end
+		end
+		return reviews
+	end
+
+	def get_score(shop_id)
+		#calculate average rating of reviews 
+		reviews = get_restaurants_review(shop_id)
+		sum = 0.0
+		reviews.each do |review|
+			sum += review["total_score"].to_d
+		end
+		sum /= reviews.size
+		score = reviews.present? ? sum : 0.0
 	end
 
 	def get_distance(lng1, lat1, lng2, lat2)
@@ -118,8 +182,26 @@ module Common
 		
 		#https://keisan.casio.jp/exec/system/1257670779
 		distance = r * Math.acos(Math.sin(y_1) * Math.sin(y_2) + Math.cos(y_1) * Math.cos(y_2) * Math.cos(x_2 - x_1)) 
-		#convert to km -> m
-		return (distance * 1000).to_i
-		
+
+	end
+
+	def get_rating(distance, score)
+=begin
+		get rating from distance and score
+		---------------
+		distance: double
+			distance in km
+			example
+			1.12
+		score: double
+			average score of reviews (0.0-5.0)
+			example
+			3.5
+		Returns
+		---------------
+		rating: doble
+			rating of 0.0 - 2.0 (distance: 1.0, score: 1.0)
+=end
+		rating = score/5.0 + 1/(1+distance**2)
 	end
 end
